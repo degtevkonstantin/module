174d7ce5ff787b74353dedb2fee4b6bd100f0091
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-01 14:19:43 +0300

    Add files via upload

--- script_4.js
+++ script_4.js
@@ -3,20 +3,26 @@
 // в которой первые два числа равны 0 и 1, а каждое последующее число равно сумме двух
 // предыдущих чисел (пример, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233).
 
-let arr = [];
 
-function create (N) {
-    while(arr.length < 2){
-        arr.push(0,1)
-    }
+const createarr = (number) => {
+    let array = [];
+    
+    if (number == 1){
+        array.push(0);
+    } else if(number >= 2){
+    array.push(0, 1);
+    } else {
+        alert('Заданно не известное значение или 0');
+    };
 
-    while(arr.length < N){
-        let a = arr.length - 1, b = arr.length - 2
-        let c = arr[a] + arr[b]
-        arr.push(c)
-    }
-}
+    while (array.length < number && 2 < number) {
+        let index1 = array.length - 1, index2 = array.length - 2
+        let arrNum = array[index1] + array[index2]
+        array.push(arrNum)
+    };
 
+    return array;
+    
+};
 
-create(7)
-console.log(arr)
+console.log(createarr(5));
