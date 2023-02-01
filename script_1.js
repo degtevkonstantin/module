d2ab39ae9f5f292cb905961bef0a1dd784e9f296
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-01 09:26:46 +0300

    Add files via upload

--- script_1.js
+++ script_1.js
@@ -11,15 +11,29 @@
 // { name: “Coffee”, total: 510 },
 // { name: “Tea”, total: 140 }]
 
-let arr = [ {name: 'Milk', price: 20, amount: 15 },
-{name:'Coffee', price: 30, amount: 17 },
-{name:'Tea', price: 10, amount: 14 } ]
-
-const newArr = arr.map((x,) => {
-    return{
-        name: x.name,
-        total: x.price * x.amount,
+const arr = [ 
+    {
+        name: 'Milk', 
+        price: 20, 
+        amount: 15 
+    },{
+        name:'Coffee', 
+        price: 30, 
+        amount: 17 
+    },{
+        name:'Tea', 
+        price: 10, 
+        amount: 14 
     }
-})
+];
 
-console.log(newArr);
+const calctotal = (array) => {
+    const resultArray = array.map((elem) => {
+        return {
+            name: elem.name,
+            total: elem.price * elem.amount
+        };
+    });
+    return resultArray;
+};
+console.log(calctotal(arr));
