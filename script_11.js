4ced7a5f95b3f8d276b0ffd7fd036f309de408a8
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-01 14:35:24 +0300

    Add files via upload

--- script_11.js
+++ script_11.js
@@ -1,10 +1,18 @@
 // Заполнить двумерный массив таблицей умножения от 0 до 10 
 // и вывести её в удобочитаемом виде.
 
+const createTable = () => {
+    let array = Array(10);
 
-let arr = Array(10);
-for (let i = 1; i < 10; i++){
-    arr[i]=[...Array(10)].map((_, j) => i * j)
-}
+    for (let i = 1; i < 10; i++) {
+        array[i] = [...Array(10)].map((_, j) => i * j);
+    };
 
-console.log(arr)
+    for (let i = 1; i < 10; i++) {
+        array[i].shift()
+    };
+
+    return array;
+};
+
+console.table(createTable());
