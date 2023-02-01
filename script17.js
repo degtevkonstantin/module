779b84cd9dff0a184f74b3fb42994e60ccb3831d
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-01 16:01:43 +0300

    Add files via upload

---
+++ script17.js
@@ -0,0 +1,15 @@
+const arr1 = [1, 2, 3, 4,];
+
+const arr2 = ['1', '2', '3', '4'];
+
+const arr3 = [{1:1},{u: 'ss'}];
+
+const calc = (...arrays) => {
+  let uniteArr = [];
+
+  uniteArr = uniteArr.concat(...arrays);
+
+  return uniteArr;
+};
+
+console.log(calc(arr1, arr2, arr3))
