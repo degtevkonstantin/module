4938a96249a12352cb7b4e466a59b9a24144ac56
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-01 14:42:15 +0300

    Add files via upload

--- script_2.js
+++ script_2.js
@@ -6,13 +6,14 @@
 
 // Output: [14, 23, 13, 7, 3]
 
-let arr = [14, 53, 23, 53, 13, 55, 7, 34, 3, 6];
+const arr = [14, 53, 23, 53, 13, 55, 7, 34, 3, 6];
+
 const filtIndex = (array) => {
     newArr = array.filter((element, index) => {
         condition = index % 2;
         condition = Boolean(boolean)
 
-        return !condition
+        return !condition;
         });
 
     return newArr;
