4598f05d2ae44e6059fa70d66b3dfd0776e23a15
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-02 14:46:45 +0300

    Add files via upload

--- script_6.js
+++ script_6.js
@@ -1,37 +1,37 @@
 //     Имеется массив пользователей вида [{ name: “Ivan”, age: 24 }].
 //     Вывести имена тех пользователей, возраст которых больше 18 лет.
 
-//     Input: [{ name: “Ivan”, age: 24 },
-//     { name: “Oleg”, age: 16},
-//     { name: “Igor”, age: 24}]
+//Input: [{ name: “Ivan”, age: 24 },
+//       { name: “Oleg”, age: 16},
+//       { name: “Igor”, age: 24}]
 // Output:
 // Ivan
 // Igor
 
 
-let arr = [
-    {
-         name: 'Ivan', 
-         age: 24 
-    },
-    { 
-        name: 'Oleg',
-        age: 16},
-    { 
-        name: 'Igor', 
-        age: 24
-    }
-]
+const arr = [
+  {
+    name: 'Ivan', 
+    age: 24 
+  },
+  { 
+    name: 'Oleg',
+    age: 16},
+  { 
+    name: 'Igor', 
+    age: 24
+  }
+];
 
 const filterArray = (array) => {
-    const filtArr = array.filter ((element,) => element.age > 18);
-    const resultArr = filtArr.map((element,) => {
-        alert(element.name);
+  const filtedArrayAge = array.filter ((element) => element.age > 18);
+  const resultArr = filtedArrayAge.map((element) => {
+    alert(element.name);
 
-        return element.name;
-    });
+    return element.name;
+  });
 
-    return resultArr;
+  return resultArr;
 };
 
 console.log(filterArray(arr));
