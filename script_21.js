89e51b675f9bb40ebc8b16a9e267724a11a5952a
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-03 16:26:26 +0300

    Add files via upload

--- script_21.js
+++ script_21.js
@@ -2,17 +2,16 @@
 // { first_name: ‘Ivan’, last_name: ‘Ivanov’, email: ‘ivanov@pochta.com’ } и возвращает строку вида:
 //  “Ivanov Ivan E-mail: ivanov@pochta.com”
 
-const Data = {
+const data = {
   first_name: 'Ivan',
   last_name: 'Ivanov',
   email: 'ivanov@pochta.com'
 };
 
-const UniteDataInString = (object) => {
-
+const uniteDataInString = (object) => {
   return (
     ''.concat (object.last_name,' ', object.first_name, ' E-mail: ', object.email,)
   );
 };
 
-console.log(UniteDataInString(Data));
+console.log(uniteDataInString(data));
