e9dced7a47ba7df425ebab20f4f7be69a77937a1
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-03 15:21:25 +0300

    Add files via upload

--- script_16.js
+++ script_16.js
@@ -5,11 +5,10 @@
 
 // Output: 8 (800) 555-35-35
 
-let nummer = '88005553535'
+const phone = '88005553535'
 
-const phoneFormat = (nummer,) => {
-
-  return nummer.replace(/(\+7|8)[\s(]?(\d{3})[\s)]?(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})/g, '8 ($2) $3-$4-$5');
+const phoneFormat = (number) => {
+  return number.replace(/(\+7|8)[\s(]?(\d{3})[\s)]?(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})/g, '8 ($2) $3-$4-$5');
 };
 
-console.log(phoneFormat(nummer))
+console.log(phoneFormat(phone))
