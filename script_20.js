35d3af5629a57bbb118c8f312c807abd31ce72a8
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-08 10:16:24 +0300

    Add files via upload

--- script_20.js
+++ script_20.js
@@ -6,31 +6,33 @@
 //  child: {
 //   path: “doodles”
 //   child: {
-//    child: {
+//     rubiks-cube
 //   }
 //  }
 // }
 
 
-const href = 'https://www.google.com/doodles/rubiks-cube'
+const href = 'https://www.google.com/doodles/rubiks-cube/'
 
 const urlInfoByObject = (url) => {
-  url = new URL (url)
+  url = new URL(url);
   arrayChilds = url.pathname.split('/');
   arrayChilds.shift()
-  child = {
-    child: {
-      path: arrayChilds[0],
-      child: arrayChilds[1]
-      
-    }
-  }
+  const child = {};
+  for (element of arrayChilds) {
+    if (child.path == undefined) {
+      child.path = element
+    } else if (child.child == undefined) {
+      child.child = {
+        child: element
+      };
+    }; 
+  };
 
-  return{
+  return {
     protocol: url.protocol,
     hostname: url.hostname,
     child
-  }
+  };
 }
-
-console.log(urlInfoByObject(href))
+console.log(urlInfoByObject(href));
