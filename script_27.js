82d326f147f0420a6a1d4fb90f0d0fd1dc4f046e
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-08 10:28:18 +0300

    Add files via upload

--- script_27.js
+++ script_27.js
@@ -1,8 +1,14 @@
+// Имеется двумерный массив. Пользуясь возможностями ES6 синтаксиса
+//  объединить его в одномерный массив.
 
-const UniteArray = (array) => {
+// Input: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const uniteArray = (array) => {
   array = array[0].concat(array[1], array[2]);
 
   return array;
 };
 
-console.log(UniteArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+console.log(uniteArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
