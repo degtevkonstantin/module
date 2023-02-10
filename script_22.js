551be12e455dc7b661c11e009115c94b5dccfdd3
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-10 10:50:53 +0300

    Add files via upload

--- script_22.js
+++ script_22.js
@@ -10,15 +10,12 @@
 
 const arr = [13, 14, 32, 23, 34, 21, 44, 47, 86];
 
-const segmentArr = (array, start, end) => {
-  if (start >= 0 && end >= start) {
-    resultArray = array.filter((_, index) => {
-      end >= index && index >= start ? true : false;
-      return resultArray;
-    });
-  } else {
-    return alert('заданны некоректные значения')
+const segmentArray = (array, start, end) => {
+  if (start < 0 || end < start || end > (array.length - 1)){
+    alert('Введены не корректные данные')
   }
+  const resultArray = array.filter((_, index) => end >= index && index >= start)
+  return resultArray;
 }
 
-console.log(segmentArr(arr, 4, 6));
+console.log(segmentArray(arr, 4, 6));
